b474636f55b08e28895e84c1911d844edab92990
Calvin Dito Pratama <calvindito7@gmail.com>
2025-10-16 22:55:01 +0700

    feat: kolom rencana ambil dan kontak koleksi dikembalikan

--- resources/views/physical-collection/collection-retur.blade.php
+++ resources/views/physical-collection/collection-retur.blade.php
@@ -122,6 +122,8 @@
                         <th class="text-nowrap"><i class="ph-gear"></i></th>
                         <th class="text-nowrap">Auto Hibah</th>
                         <th class="text-nowrap">Status</th>
+                        <th class="text-nowrap">Rencana Ambil</th>
+                        <th class="text-nowrap">Kontak</th>
                         <th class="text-nowrap">Judul</th>
                         <th class="text-nowrap">Pelaksana Serah</th>
                         <th class="text-nowrap">Tujuan</th>
@@ -203,8 +205,10 @@
                 { orderable: false, className: 'align-middle text-center allow-select' },
                 { orderable: true, className: 'align-middle text-center allow-select' },
                 { orderable: false, className: 'align-middle text-center' },
+                { orderable: false, className: 'align-middle allow-select' },
                 { orderable: false, className: 'align-middle' },
-                { orderable: false, className: 'align-middle' },
+                { orderable: true, className: 'align-middle text-wrap allow-select' },
+                { orderable: true, className: 'align-middle allow-select' },
                 { orderable: true, className: 'align-middle text-wrap allow-select' },
                 { orderable: true, className: 'align-middle text-wrap allow-select' },
                 { orderable: true, className: 'align-middle text-wrap allow-select' },
