7fe67f9b02d5145ce602ee82c02032aa0a82ca68
Programmer <calvindito7@gmail.com>
2026-02-24 13:30:40 +0700

    update server

--- config/inlis.php
+++ config/inlis.php
@@ -4,6 +4,7 @@ return [
     'domain' => env('INLIS_DOMAIN'),
     'token' => env('INLIS_TOKEN'),
     'base_url' => env('INLIS_BASE_URL'),
+    'inlis_url' => env('INLIS_URL'),
     'api_url' => env('INLIS_API_URL'),
     'aes_key' => null,
     'aes_iv' => null,
