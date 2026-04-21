b9cbb09d671db1162600a8cfc35b8fca73f1889a
Okta <isbngithub@gmail.com>
2026-04-21 15:28:25 +0700

    Update SingleVerificationController.php

--- app/Http/Controllers/PhysicalDelivery/SingleVerificationController.php
+++ app/Http/Controllers/PhysicalDelivery/SingleVerificationController.php
@@ -239,8 +239,8 @@ class SingleVerificationController extends Controller
                         ], false);
                 } else {
                     QueryAPI::update('letter', $request->letter_id, [
-                            'status' => $status,
-                            'accept_date' => $request->received_date,
+                            //'status' => $status,
+                            //'accept_date' => $request->received_date,
                             'update_by' => session('username'),
                             'update_terminal' => $request->ip(),
                             'update_date' => date('Y-m-d')
